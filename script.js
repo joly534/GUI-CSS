51bb3a5158baf1e2c02cee26ce6da91c8b8fa467
jonathan joly <41478399+joly534@users.noreply.github.com>
2021-07-01 19:24:08 +0200

    update waiting on textshadow

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let text = document.getElementById('text');
 let vue = document.getElementById('vue');
-
+let rgbcolors = ["125","125","125"];
+let textShadows = ["5","5","5"];
 
 function validate(){
 	vue.innerHTML='';
@@ -25,31 +26,45 @@ function changeFontWeight(valueWeightFont){
 	vue.style.fontWeight = valueWeightFont;
 }
 
-function changeFontColor(valueColorFont){
-	
-	vue.style.color = valueColorFont;
+/** FONCTIONS DE PARAMETRAGE DE LA COULEUR DU TEXTE */
+function setRgbColorFontOne(valueColorFont){
+	rgbcolors.splice(0,1,valueColorFont);
 }
+function setRgbColorFontTwo(valueColorFont){
+	rgbcolors.splice(1,1,valueColorFont);
+}
+function setRgbColorFontThree(valueColorFont){
+	rgbcolors.splice(2,1,valueColorFont);	
+}
+function changeFontRgbColor(){
+	let rgb = document.getElementById('RGB');
+	rgb.innerHTML = "rgb(" + rgbcolors[0] + "," + rgbcolors[1] + "," + rgbcolors[2] + ")";
+	vue.style.color ="rgb(" + rgbcolors[0] + "," + rgbcolors[1] + "," + rgbcolors[2] + ")";
+}
+/** FIN */
 
-// function changeFontRgbColorOne(valueColorFont){
-// 	let rgbOne = (valueColorFont).toString();
-// 	return rgbOne;
-// }
-// function changeFontRgbColorTwo(valueColorFont){
-// 	let rgbTwo = (valueColorFont).toString();
-// 	return rgbTwo;
-// }
-// function changeFontRgbColorThree(valueColorFont){
-// 	let rgbThree = (valueColorFont).toString();
-// 	return rgbThree;
-// }
-// function changeFontRgbColor(){
-// 	let rgbOne = changeFontRgbColorOne(valueColorFont);
-// 	let rgbOne = changeFontRgbColorTwo(valueColorFont);
-// 	let rgbOne = changeFontRgbColorThree(valueColorFont);
-// 	vue.style.color ="rgb(" + rgbOne + "," + rgbTwo + "," + rgbThree + ")";
-// }
-
+/** FONCTIONS DE PARAMETRAGE DU TEXT-SHADOW */
+function setTextShadowX(valueShadowX){
+	textShadows.splice(0,1,valueShadowX);
+}
+function setTextShadowY(valueShadowY){
+	textShadows.splice(1,1,valueShadowY);
+}
+function setTextShadowBlur(valueShadowBlur){
+	textShadows.splice(2,1,valueShadowBlur);
+}
+function changeTextShadow(){
+	let text_shadow = document.getElementById('text_shadow');
+	text_shadow.innerHTML = '"' + textShadows[0] + 'px ' + textShadows[1] + 'px ' + textShadows[2] + 'px black"';
+	vue.style.textShadow = '"' + textShadows[0] + 'px ' + textShadows[1] + 'px ' + textShadows[2] + 'px black"';
+	console.log(textShadows);
+}
+/** FIN */
 
 function update() {
+	changeFontRgbColor();
+	changeTextShadow();
+window.requestAnimationFrame(update);
 }
-update()
+
+window.requestAnimationFrame(update);
