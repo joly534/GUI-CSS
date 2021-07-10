39630d651218899ab36650b7ba2eff08c7639835
jonathan joly <41478399+joly534@users.noreply.github.com>
2021-07-10 11:51:59 +0200

    update

--- functions/functions.js
+++ functions/functions.js
@@ -1,13 +1,25 @@
 
-    let ongletText = document.getElementById('onglet_text');
+let ongletText = document.getElementById('onglet_text');
 let ongletBorder = document.getElementById('onglet_border');
 let ongletBox = document.getElementById('onglet_box');
+let titleOngletText = document.getElementById('ongletText');
+
+ongletText.style.display = "block";
 
 function showOngletText(){
     
     ongletBorder.style.display = "none";
     ongletBox.style.display = "none";
     ongletText.style.display = "block";
+    titleOngletText.style.borderRightColor = "aqua";
+    titleOngletText.style.borderTopColor = "aqua";
+    titleOngletText.style.borderLeftColor = "aqua";
+    titleOngletText.style.borderTopWidth = "2px";
+    titleOngletText.style.borderRightWidth = "2px";
+    titleOngletText.style.borderLeftWidth = "2px";
+    titleOngletText.style.borderTopStyle = "outset";
+    titleOngletText.style.borderRightStyle = "outset";
+    titleOngletText.style.borderLeftStyle = "outset";
 }
 function showOngletBorder(){
     ongletText.style.display = "none";
